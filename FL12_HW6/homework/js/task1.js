289c16032e6c3aa9b58f2951877d1af6258c4a0f
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2019-12-20 21:31:18 +0200

    int -> float, add round

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,6 +1,6 @@
-let a = parseInt(prompt('Pleace enter the first number', ''));
-let b = parseInt(prompt('Pleace enter the second number', ''));
-let c = parseInt(prompt('Pleace enter the third number', ''));
+let a = parseFloat(prompt('Pleace enter the first number', ''));
+let b = parseFloat(prompt('Pleace enter the second number', ''));
+let c = parseFloat(prompt('Pleace enter the third number', ''));
 
 let num2 = 2;
 let num4 = 4;
@@ -8,8 +8,8 @@ let num4 = 4;
 let discr = b * b - num4 * a * c;
 let sqrtDiscr = Math.sqrt(discr);
 
-let x1 = (-b + sqrtDiscr) / num2 * a;
-let x2 = (-b - sqrtDiscr) / num2 * a;
+let x1 = Math.round((-b + sqrtDiscr) / (num2 * a));
+let x2 = Math.round((-b - sqrtDiscr) / (num2 * a));
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     console.log('Invalid input data');
@@ -21,4 +21,4 @@ if (isNaN(a) || isNaN(b) || isNaN(c)) {
     } else {
         console.log(`x1 = ${x1} and x2 = ${x2}`);
     }
-}
+} 
