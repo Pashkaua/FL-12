4d0c4be3832a940f1596bbf619d3c9061d4b18c0
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-01-10 22:01:40 +0200

    change after q&a, fix magic number and add invoke function

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,5 +1,8 @@
 // Task 1 converts the argument values.
 
+let aOne = 2;
+let bOne = 3;
+
 function converts(...args) {
     let res = [];
 
@@ -15,8 +18,14 @@ function converts(...args) {
     return res;
 }
 
+converts('1', aOne, bOne, '4');
+
 
 // Task 2  iterates over array 
+
+let aTwo = 1;
+let bTwo = 2;
+let cTwo = 3;
 
 function executeforEach(arr, fun) {
     for (let i = 0; i < arr.length; i++) {
@@ -24,8 +33,16 @@ function executeforEach(arr, fun) {
     }
 }
 
+executeforEach([aTwo, bTwo, cTwo], function (el) {
+    console.log(el * bTwo)
+});
+
 
 // Task 3  mapArray
+
+let aThree = 2;
+let bThree = 3;
+let cThree = 8;
 
 function mapArray(arr, fun) {
     let res = [];
@@ -35,8 +52,16 @@ function mapArray(arr, fun) {
     return res;
 }
 
+mapArray([aThree, '5', cThree], function (el) {
+    return el + bThree;
+});
+
 
 // Task 4 filterArray
+
+let aFour = 2;
+let bFour = 5;
+let cFour = 8;
 
 function filterArray(arr, fun) {
     let res = [];
@@ -47,6 +72,10 @@ function filterArray(arr, fun) {
     });
     return res;
 }
+
+filterArray([aFour, bFour, cFour], function (el) {
+    return el % aFour === 0;
+});
 
 
 //Task 5 reverses the string value 
@@ -59,8 +88,13 @@ function flipOver(str) {
     return res;
 }
 
+flipOver('hey world');
+
 
 //Task 6 range of numbers
+
+const aSix = 2;
+const bSix = 7;
 
 function makeListFromRange(arr) {
     let res = [];
@@ -69,6 +103,8 @@ function makeListFromRange(arr) {
     }
     return res;
 }
+
+makeListFromRange([aSix, bSix]);
 
 
 //Task 7 get keys of array
@@ -81,8 +117,20 @@ function getArrayOfKeys(arr, val) {
     return res;
 }
 
+const actors = [
+    { name: 'tommy', age: 36 },
+    { name: 'lee', age: 28 }
+];
+
+getArrayOfKeys(actors, 'name');
+
 
 //Task 8 get keys of array
+
+const aEight = 58;
+const bEight = 14;
+const cEight = 48;
+const dEight = 2;
 
 function substitute(arr) {
     const max = 30;
@@ -96,28 +144,38 @@ function substitute(arr) {
     });
 }
 
+substitute([aEight, bEight, cEight, dEight]);
+
 
 //Task 9 get  past day
+
+const aNine = 2;
+const bNine = 365;
+const cNine = 2019;
 
 function getPastDay(date, day) {
 
     let new_date = new Date(date);
-
     let res = new Date(new_date.setDate(new_date.getDate() - day));
 
-    const options = { day: 'numeric', month: 'short', year: 'numeric' };
-
-    return res.toLocaleString('en-GB', options);
+    return res.getDate();
 }
+
+const date = new Date(cNine, 0, aNine);
+
+getPastDay(date, 1);
+getPastDay(date, aNine);
+getPastDay(date, bNine);
 
 
 //Task 10 format Date
 
 function formatDate(date) {
-    let options = {
-        year: 'numeric', month: 'numeric', day: '2-digit',
-        hour: 'numeric', minute: 'numeric', hour12: false
-    };
 
-    return date.toLocaleString('zh-CN', options);
+    let res = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+
+    return res;
 }
+
+formatDate(new Date('6/15/2018 09:15:00'));
+formatDate(new Date());
