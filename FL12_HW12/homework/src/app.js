8648ddca33e07aec6139b5a5908f01e916008198
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-01-28 21:46:24 +0200

    fin_ver

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -23,6 +23,7 @@ let list = [];
 
 function rendMain() {
     rootNode.innerHTML = '';
+    const zero = 0;
 
     let currentList = JSON.parse(localStorage.getItem('list'));
     if (currentList) {
@@ -40,37 +41,39 @@ function rendMain() {
 
     const ul = document.createElement('ul');
 
-    if (list && list.length) {
+    if (list.length) {
         for (let i = 0; i < list.length; i++) {
 
-            const li = document.createElement('li');
-            const pTerm = document.createElement('p');
-            pTerm.innerHTML = list[i].term;
-            const pDef = document.createElement('p');
-            pDef.innerHTML = list[i].def;
-            if (list[i].check) {
-                pTerm.className = 'done';
-                pDef.className = 'done';
+            if (list[i].terms.length) {
+                const li = document.createElement('li');
+                const pTerm = document.createElement('p');
+                pTerm.innerHTML = list[i].terms[zero].term;
+                const pDef = document.createElement('p');
+                pDef.innerHTML = list[i].terms[zero].def;
+                if (list[i].check) {
+                    pTerm.className = 'done';
+                    pDef.className = 'done';
+                }
+                pTerm.addEventListener('click', () => markDone(i));
+                pDef.addEventListener('click', () => markDone(i));
+
+                const btnEdit = document.createElement('button');
+                btnEdit.className = 'btn-save';
+                btnEdit.innerHTML = 'edit';
+                btnEdit.addEventListener('click', function () {
+                    window.location.hash = `modify/id:${i}`
+                });
+
+                const btnRemove = document.createElement('button');
+                btnRemove.className = 'btn-cancel';
+                btnRemove.innerHTML = 'remove';
+                btnRemove.addEventListener('click', () => remove(i));
+                li.appendChild(pTerm);
+                li.appendChild(pDef);
+                li.appendChild(btnEdit);
+                li.appendChild(btnRemove);
+                ul.appendChild(li);
             }
-            li.appendChild(pTerm);
-            li.appendChild(pDef);
-            pTerm.addEventListener('click', () => markDone(i));
-            pDef.addEventListener('click', () => markDone(i));
-
-            const btnEdit = document.createElement('button');
-            btnEdit.className = 'btn-save';
-            btnEdit.innerHTML = 'edit';
-            btnEdit.addEventListener('click', function () {
-                window.location.hash = `modify/id:${i}`
-            });
-            li.appendChild(btnEdit);
-
-            const btnRemove = document.createElement('button');
-            btnRemove.className = 'btn-cancel';
-            btnRemove.innerHTML = 'remove';
-            btnRemove.addEventListener('click', () => remove(i));
-            li.appendChild(btnRemove);
-            ul.appendChild(li);
         }
     }
 
@@ -84,16 +87,33 @@ function rendAddNew() {
 
     const div = document.createElement('div');
     div.className = 'block-add-new';
-    const inputTerm = document.createElement('input');
-    const inputDef = document.createElement('input');
+    const inputName = document.createElement('input');
+    inputName.setAttribute('type', 'text');
+    inputName.setAttribute('placeholder', 'Name');
+    inputName.className = 'term-name';
+    div.appendChild(inputName);
+
+    const btnAdd = document.createElement('button');
     const btnEdit = document.createElement('button');
     const btnCancel = document.createElement('button');
-    inputTerm.className = 'term';
-    inputTerm.setAttribute('placeholder', 'Enter term');
-    div.appendChild(inputTerm);
-    inputDef.className = 'definition';
-    inputDef.setAttribute('placeholder', 'Enter definition');
-    div.appendChild(inputDef);
+
+    btnAdd.className = 'btn-add';
+    btnAdd.innerHTML = 'cancel';
+    btnAdd.innerHTML = 'Add terms';
+    div.appendChild(btnAdd);
+    btnAdd.addEventListener('click', () => {
+        if (!inputName.value) {
+            alert('Pleace enter the name')
+        } else {
+            let set = {
+                'mame': inputName.value,
+                terms: []
+            }
+            rendNewTerm(set);
+        }
+
+    });
+
     btnEdit.className = 'btn-save';
     btnEdit.innerHTML = 'Save changes';
     div.appendChild(btnEdit);
@@ -107,7 +127,44 @@ function rendAddNew() {
     rootNode.appendChild(div);
 }
 
-function pushToLocal() {
+function rendNewTerm(set) {
+
+    console.log(set);
+
+    rootNode.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.className = 'block-add-new';
+    const inputTerm = document.createElement('input');
+    inputTerm.setAttribute('type', 'text');
+    inputTerm.setAttribute('placeholder', 'Enter term');
+    const inputDef = document.createElement('input');
+    inputDef.setAttribute('type', 'text');
+    inputDef.setAttribute('placeholder', 'Enter definition');
+    const btnEdit = document.createElement('button');
+    const btnCancel = document.createElement('button');
+    inputTerm.className = 'term';
+    div.appendChild(inputTerm);
+    inputDef.className = 'definition';
+    div.appendChild(inputDef);
+
+    btnEdit.className = 'btn-save';
+    btnEdit.innerHTML = 'Save changes';
+    div.appendChild(btnEdit);
+
+    btnCancel.className = 'btn-cancel';
+    btnCancel.innerHTML = 'cancel';
+    btnCancel.addEventListener('click', function () {
+        window.location.hash = '';
+    });
+    div.appendChild(btnCancel);
+    btnEdit.addEventListener('click', () => {
+        pushToLocal(set);
+    });
+    rootNode.appendChild(div);
+}
+
+function pushToLocal(set) {
     const term = document.querySelector('.term').value;
     const def = document.querySelector('.definition').value;
 
@@ -133,36 +190,44 @@ function pushToLocal() {
                 return;
             }
         }
+        set.terms.push(newItem);
+        console.log(set);
 
-        list.push(newItem);
+        list.push(set);
         localStorage.setItem('list', JSON.stringify(list));
         window.location.hash = '';
     }
 }
 
 function modify(el) {
+    console.log(list);
+    console.log(el);
+    const zero = 0;
+
     rootNode.innerHTML = '';
 
     const div = document.createElement('div');
     div.className = 'block-add-new';
     const inputTerm = document.createElement('input');
+    inputTerm.setAttribute('type', 'text');
     const inputDef = document.createElement('input');
+    inputTerm.setAttribute('type', 'text');
     const btnEdit = document.createElement('button');
     const btnCancel = document.createElement('button');
 
     inputTerm.className = 'term';
-    inputTerm.value = list[el].term;
+    inputTerm.value = list[el].terms[zero].term;
     div.appendChild(inputTerm);
 
     inputDef.className = 'definition';
-    inputDef.value = list[el].def;
+    inputDef.value = list[el].terms[zero].def;
     div.appendChild(inputDef);
 
     btnEdit.className = 'btn-save';
     btnEdit.innerHTML = 'Save changes';
     btnEdit.addEventListener('click', function () {
-        list[el].term = inputTerm.value;
-        list[el].def = inputDef.value;
+        list[el].terms[zero].term = inputTerm.value;
+        list[el].terms[zero].def = inputDef.value;
         localStorage.setItem('list', JSON.stringify(list));
         window.location.hash = '';
     });
