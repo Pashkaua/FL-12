a806b2eccdb0dc614c64adb6362333784a7aaac8
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-03 13:43:49 +0200

    change plugin

--- FL12_Webinar_HW6/task2/js/index.js
+++ FL12_Webinar_HW6/task2/js/index.js
@@ -18,113 +18,118 @@ let todos = [
   // }
 ];
 
-if (list) {
-  todos = JSON.parse(list);
-  createList();
-}
-
-
-function createList() {
-
-  $(todos).each(
-    function (i, el) {
-      if (el.done) {
-        $doneClass = 'done';
-      } else {
-        $doneClass = '';
-      }
-      $(".list").append(
-        `<li class="item">
-          <span class="item-text ${$doneClass}" >${el.text}</span>
-          <button class="item-remove">Remove</button>
-        </li>`)
-    }
-  )
-}
-
-
-function newItem() {
-  let todoItem = $input.val();
-  if (!todoItem) {
-    alert(`Pleace enter something`);
-  } else {
-    let item = {
-      text: todoItem,
-      done: false
-    }
-    todos.push(item)
-    $(".list").append(
-      `<li class="item">
-        <span class="item-text" >${todoItem}</span>
-        <button class="item-remove">Remove</button>
-      </li>`)
-    $input.val("");
-  }
-  $().saveToLocal();
-}
-
-
-function deleteTodoItem(e, item, index) {
-  e.preventDefault();
-  $(item).parent().fadeOut('slow', function () {
-    $(item).parent().remove();
-  });
-  todos.splice(index, 1);
-  $().saveToLocal();
-}
-
-
-function completeTodoItem() {
-  $(this).toggleClass("done");
-  let index = $list.children().index($(this).parent());
-  todos[index].done ? todos[index].done = false : todos[index].done = true;
-  $().saveToLocal();
-}
-
-
-function search(str) {
-  $searchInp.val("");
-  if (!str) {
-    alert('Pleace enter something')
-  } else {
-    $(todos).each(function () {
-      $("span:contains('" + str + "')").css("border", "1px solid red");
-      setTimeout(function () {
-        $("span:contains('" + str + "')").css("border", "none");
-      }, 2000);
-    })
-  }
-}
-
-
-$(function () {
-
-  $add.on('click', function (e) {
-    e.preventDefault();
-    newItem();
-  });
-
-  $list.on('click', '.item-remove', function (e) {
-    let item = this;
-    let index = $list.children().index($(item).parent());
-    deleteTodoItem(e, item, index);
-  })
-
-  $searchBtn.on('click', function (e) {
-    e.preventDefault();
-    search($searchInp.val());
-  }
-  )
-
-  $(document).on('click', ".item-text", completeTodoItem)
-
-});
-
 
 (function ($) {
-  $.fn.saveToLocal = function () {
+  $.fn.todo = function () {
 
-    localStorage.setItem("list", JSON.stringify(todos));
+    if (list) {
+      todos = JSON.parse(list);
+      createList();
+    }
 
+    function createList() {
+
+      $(todos).each(
+        function (i, el) {
+          if (el.done) {
+            $doneClass = 'done';
+          } else {
+            $doneClass = '';
+          }
+          $(".list").append(
+            `<li class="item">
+            <span class="item-text ${$doneClass}" >${el.text}</span>
+            <button class="item-remove">Remove</button>
+          </li>`)
+        }
+      )
+    }
+
+
+    function newItem() {
+      let todoItem = $input.val();
+      if (!todoItem) {
+        alert(`Pleace enter something`);
+      } else {
+        let item = {
+          text: todoItem,
+          done: false
+        }
+        todos.push(item)
+        $(".list").append(
+          `<li class="item">
+          <span class="item-text" >${todoItem}</span>
+          <button class="item-remove">Remove</button>
+        </li>`)
+        $input.val("");
+      }
+      saveToLocal();
+    }
+
+
+    function deleteTodoItem(e, item, index) {
+      e.preventDefault();
+      $(item).parent().fadeOut('slow', function () {
+        $(item).parent().remove();
+      });
+      todos.splice(index, 1);
+      saveToLocal();
+    }
+
+
+    function completeTodoItem() {
+      $(this).toggleClass("done");
+      let index = $list.children().index($(this).parent());
+      todos[index].done ? todos[index].done = false : todos[index].done = true;
+      saveToLocal();
+    }
+
+
+    function search(str) {
+      $searchInp.val("");
+      if (!str) {
+        alert('Pleace enter something')
+      } else {
+        $(todos).each(function () {
+          $("span:contains('" + str + "')").css("border", "1px solid red");
+          setTimeout(function () {
+            $("span:contains('" + str + "')").css("border", "none");
+          }, 2000);
+        })
+      }
+    }
+
+
+    $(function () {
+
+      $add.on('click', function (e) {
+        e.preventDefault();
+        newItem();
+      });
+
+      $list.on('click', '.item-remove', function (e) {
+        let item = this;
+        let index = $list.children().index($(item).parent());
+        deleteTodoItem(e, item, index);
+      })
+
+      $searchBtn.on('click', function (e) {
+        e.preventDefault();
+        search($searchInp.val());
+      }
+      )
+
+      $(document).on('click', ".item-text", completeTodoItem)
+
+    });
+
+
+    function saveToLocal() {
+
+      localStorage.setItem("list", JSON.stringify(todos));
+
+    };
   };
 })(jQuery);
+
+$().todo();
