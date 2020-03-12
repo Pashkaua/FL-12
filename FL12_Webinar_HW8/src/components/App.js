d7ac5f91abc9c98f6cad2b8c7e6490343ea9c692
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-12 17:15:33 +0200

    added search function

--- FL12_Webinar_HW8/src/components/App.js
+++ FL12_Webinar_HW8/src/components/App.js
@@ -4,16 +4,18 @@ import List from './RenderList';
 import { useSelector } from 'react-redux'
 import '../styles/App.css';
 
+import { useLocation } from "react-router-dom";
 
 export default function App() {
 
+  const data = useLocation();
   const courses = useSelector(state => state.listCourses)
 
   return (
     <div className="app">
       <Header />
 
-      <List list={courses} />
+      <List list={data.list ? data.list.filtered : courses} />
     </div>
   )
 }
--- FL12_Webinar_HW8/src/components/Header.js
+++ FL12_Webinar_HW8/src/components/Header.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 
 function Header() {
+    const history = useHistory();
+    const item = useSelector(state => state.listCourses)
+
+    const search = (e) => {
+        e.preventDefault();
+
+        const filtered = item.filter((element) => {
+            const regex = RegExp("^" + e.target.value.trim() + "");
+            return regex.test(element.title)
+        })
+        history.push({
+            pathname: '/',
+            list: { filtered }
+        }
+        )
+    }
     return (
-        <div className="header">
-            <input id="inp-search" type="text" placeholder="search" />
+        <div>
+            <div className="header">
+                <input id="inp-search" type="text" onChange={search} placeholder="search" />
 
-            <Link id="link-add" to='/add'>Add course</Link>
+                <Link id="link-add" to='/add'>Add course</Link>
+            </div>
+            <div>
+            </div>
         </div>
-
-
     );
 }
 
