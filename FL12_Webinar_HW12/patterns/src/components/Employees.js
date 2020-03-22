36038d50ed76182f99e2a5994c7ca4f593700e87
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-22 22:14:44 +0200

    add hw_webinar_12_patterns v_01

---
+++ FL12_Webinar_HW12/patterns/src/components/Employees.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class Employees extends Component {
+  render() {
+    const item = this.props.item;
+    const filter = this.props.filter;
+    const employee = (item, index) => {
+      return (
+        <li key={index}>
+          {item.performance !== "top" &&
+          (item.performance === filter || !filter) ? (
+            <div className="block-item">
+              <p>{item.name}</p>
+              <p> Performance: {item.performance}</p>
+              <p>Last vacation date: {item.last_vacation_date}</p>
+              <p>Salary: {item.salary}</p>{" "}
+            </div>
+          ) : null}
+
+          {item.children ? (
+            <ul>
+              {item.children.map((child, index) => employee(child, index))}{" "}
+            </ul>
+          ) : null}
+        </li>
+      );
+    };
+    return employee(item);
+  }
+}
